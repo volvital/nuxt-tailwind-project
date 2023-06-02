4e238fd2ca8cf4152e883d108ba9dcf619ec9bd3
VolVital <98488542+volvital@users.noreply.github.com>
2023-06-03 00:26:52 +0300

    Updated interface

--- app.vue
+++ app.vue
@@ -38,7 +38,7 @@
             leave-to="opacity-0 scale-95"
           >
             <DialogPanel
-              class="aspect-h-1 max-w-full transform overflow-hidden shadow-xl transition-all text-white"
+              class="min-w-full transform overflow-hidden shadow-xl transition-all text-white"
             >
               <div class=" text-end">
                 <button 
@@ -55,88 +55,95 @@
                   </svg>
                 </button>
               </div>
-              <div class=" text-center">
-                <div class="flex justify-between mt-16 mb-3 ml-20 mr-20 text-3xl/10 leading-6">
-                  <div>Kor Blockchain Badge</div>
-                  <div>#{{ currentIdImage }}</div>
-                </div>           
-              
-                <div class="text-sm underline decoration-white mb-5 ml-20">
-                  <ul class="flex sm:flex-none">
-                    <li class="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 20" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                        <path
-                          stroke-linecap="round" 
-                          stroke-linejoin="round" 
-                          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                      </svg>
-                      <a class="mr-6" href="#">Owner</a>
-                    </li>
-                    <li class="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                        <path 
-                          stroke-linecap="round" 
-                          stroke-linejoin="round" 
-                          d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
-                      </svg>
-                      <a class="mr-6" href="#">Collection</a>
-                    </li>
-                    <li class="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
-                      </svg>
-                      <a class="mr-6" @click="copyToClipboard">Share</a>
-                    </li>
-                  </ul>
-                </div> 
-              </div>
-              
-                <div  class="text-center">
-                  <div class="flex items-center m-auto text-center">
-                    <button 
-                      :disabled="currentDisabledPrevious"
-                      @click="decrementId(currentIdImage)" 
-                      class="h-14 w-14 relative inline-flex items-center rounded-2xl px-6 py-3 bg-gray-50/[.15] hover:bg-gray-500 focus:z-20 focus:outline-offset-0 mr-5"                  
-                    >
-                      <span class="sr-only">Previous</span>
-                      <svg class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path
-                          fill-rule="evenodd"
-                          d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
-                          clip-rule="evenodd"
-                          />
-                      </svg>
-                    </button>
-                    <div class="overflow-hidden rounded-2xl xl:aspect-h-8 xl:aspect-w-7">                
-                      <img
-                        :src="currentImageURL"
-                        :alt="image.infoUrl" class="h-full w-full object-cover object-center group-hover:opacity-75" />                
+              <div class=" flex justify-center">
+                <DialogTitle class="w-full max-w-xl">
+                  <div class=" text-center">
+                    <div class="flex justify-between mt-16 mb-3 ml-20 mr-20 text-3xl/10 leading-6">
+                      <div>Kor Blockchain Badge</div>
+                      <div>#{{ currentIdImage }}</div>
                     </div>
+                  </div>
+                  <div class=" flex items-center">
+                    <div class="text-sm underline decoration-white mb-5 ml-20">
+                      <ul class="flex space-x-5">
+                        <li class="flex space-x-1">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 20" stroke-width="1.5" stroke="currentColor" class="w-3.5 h-3.5 mt-1">
+                            <path
+                              stroke-linecap="round" 
+                              stroke-linejoin="round" 
+                              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                          </svg>
+                          <a href="#">Owner</a>
+                        </li>
+                        <li class="flex space-x-1">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3.5 h-3.5 mt-1">
+                            <path 
+                              stroke-linecap="round" 
+                              stroke-linejoin="round" 
+                              d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
+                          </svg>
+                          <a href="#">Collection</a>
+                        </li>
+                        <li class="flex space-x-1">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3.5 h-3.5 mt-1">
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+                          </svg>
+                          <a @click="copyToClipboard">Share</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  
+                  <div  class=" flex items-center">
+                    <div class="flex items-center text-center">
+                      <button 
+                        :disabled="currentDisabledPrevious"
+                        @click="previousImage(currentIdImage)" 
+                        class="h-14 w-14 inline-flex items-center rounded-2xl px-6 py-3 bg-gray-50/[.15] hover:bg-gray-500 focus:z-20 focus:outline-offset-0 mr-5"                  
+                      >
+                        <span class="sr-only">Previous</span>
+                        <svg class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path
+                            fill-rule="evenodd"
+                            d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                            clip-rule="evenodd"
+                            />
+                        </svg>
+                      </button>
+                      <div class="overflow-hidden rounded-2xl xl:aspect-h-8 xl:aspect-w-7">                
+                        <img
+                          :src="currentImageURL"
+                          :alt="image.infoUrl" 
+                          class="h-full w-full object-cover object-center group-hover:opacity-75" />                
+                      </div>
+                      <button
+                        :disabled="currentDisabledNext"  
+                        @click="nextImage(currentIdImage)" 
+                        class="h-14 w-14 relative inline-flex items-center rounded-2xl px-6 py-6 bg-gray-50/[.15] hover:bg-gray-500 focus:z-20 focus:outline-offset-0 ml-5"
+                      >
+                        <span class="sr-only">Next</span>
+                        <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path
+                            fill-rule="evenodd" 
+                            d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" 
+                            clip-rule="evenodd" />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                
+                  <div class="m-auto mt-4 mb-12 text-center">
                     <button
-                      :disabled="currentDisabledNext"  
-                      @click="incrementId(currentIdImage)" 
-                      class="h-14 w-14 relative inline-flex items-center rounded-2xl px-6 py-6 bg-gray-50/[.15] hover:bg-gray-500 focus:z-20 focus:outline-offset-0 ml-5"
+                      type="button"
+                      class="h-14 w-55 py-5.5 px-9 text-sm rounded-2xl bg-gray-50/[.15] font-medium hover:bg-gray-500 focus:outline-offset-0"                  
                     >
-                      <span class="sr-only">Next</span>
-                      <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path
-                          fill-rule="evenodd" 
-                          d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" 
-                          clip-rule="evenodd" />
-                      </svg>
+                      OPEN MARKETPLACE
                     </button>
                   </div>
-                </div>
-              <div class="m-auto mt-4 mb-12 text-center">
-                <button
-                  type="button"
-                  class="h-14 w-55 py-5.5 px-9 text-sm rounded-2xl bg-gray-50/[.15] font-medium hover:bg-gray-500 focus:outline-offset-0"                  
-                >
-                  OPEN MARKETPLACE
-                </button>
+                </DialogTitle>
               </div>
             </DialogPanel>
           </TransitionChild>
@@ -215,34 +222,34 @@ export default {
       return currentURL
     },
     currentDisabledPrevious() {
-      return this.currentIdImage === this.JSON[0].id ? true : false
+      return this.currentIdImage === this.JSON[0].id
     },
     currentDisabledNext() {
-      return this.currentIdImage === this.JSON[this.JSON.length - 1].id ? true : false
+      return this.currentIdImage === this.JSON[this.JSON.length - 1].id
     }
   },
   methods: {
     closeModal() {
       this.isOpen = false;
     },
-    setImage(id, JSON) {
-      let image = JSON.find(item => item.id == id);
+    setImage(id) {
+      let image = this.JSON.find(item => item.id === id);
       this.image = image;
     },
     openModal(id) {
       this.isOpen = true;
       this.id = id;
-      this.setImage(id, this.JSON);
+      this.setImage(this.id);
     },
-    incrementId(id) {
-      id++;
-      this.id = id;
-      this.setImage(id, this.JSON);
+    previousImage(id) {
+      let index = this.JSON.findIndex(item => item.id === id);
+      this.id = this.JSON[index - 1].id;
+      this.setImage(this.id);
     },
-    decrementId(id) {
-      id--;
-      this.id = id;
-      this.setImage(id, this.JSON);
+    nextImage(id) {
+      let index = this.JSON.findIndex(item => item.id === id);
+      this.id = this.JSON[index + 1].id;
+      this.setImage(this.id);
     },
     copyToClipboard() {        
       copyText(this.currentPageURL, undefined, (error, event) => {
