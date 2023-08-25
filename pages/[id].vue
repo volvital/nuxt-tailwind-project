dcde255c88af1a626504c5e13e4516c3f20cd45f
VolVital <98488542+volvital@users.noreply.github.com>
2023-08-25 11:43:14 +0300

    Added open images at own address

--- pages/[id].vue
+++ pages/[id].vue
@@ -13,7 +13,7 @@
       <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 xl:gap-x-8">
         <a v-for="image in images" :key="image.id" class="group">
           <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img :src="image.imageURL" :alt="image.infoUrl" class="h-full w-full object-cover object-center group-hover:opacity-75" />
+            <img :src="image.imageURL" :alt="image.title" class="h-full w-full object-cover object-center group-hover:opacity-75" />
           </div>
           <h3 class="mt-4 text-sm text-gray-700">Kor Blockchain OG Badge</h3>
           <p class="mt-1 text-lg font-medium text-gray-900">#{{ image.id }}</p>
@@ -113,7 +113,7 @@
                       <div class="overflow-hidden rounded-2xl">                
                         <img
                           :src="dogsStore.image.imageURL"
-                          :alt="dogsStore.image.infoUrl" 
+                          :alt="dogsStore.image.title" 
                           class="object-cover object-center" />                
                       </div>
 											<NuxtLink :to="`/${nextImage}`" :disabled="currentDisabledNext">
